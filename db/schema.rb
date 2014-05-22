0e9a0f5d3148124e97190b6d12264f704cc4bea0
vagrant <vagrant@vagrant-fedora-19.vagrantup.com>
2014-05-22 16:49:25 +0000

    add room table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522164356) do
+ActiveRecord::Schema.define(version: 20140522164820) do
 
   create_table "entries", force: true do |t|
     t.integer  "user_id"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20140522164356) do
     t.integer  "period_id"
     t.string   "day"
     t.integer  "lecture_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rooms", force: true do |t|
+    t.string   "name"
+    t.string   "spot"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

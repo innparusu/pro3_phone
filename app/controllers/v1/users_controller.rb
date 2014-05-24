683a197216596bb3126f2099c90f3425ab174539
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-24 13:35:16 +0900

    Add usersPostApi variable

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -3,7 +3,11 @@ module V1
     protect_from_forgery with: :null_session
     def create
       @user = User.new
-      @user.name = params[:name]
+      @user.name         = params[:name]
+      @user.grade        = params[:grade].to_i
+      @user.number       = params[:number]
+      @user.mail_address = params[:mail_address]
+      @user.password     = params[:password]
       @user.save
       respond_to do |format|
         format.json { render json: @user}

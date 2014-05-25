9d9525041972d2463583b62a827150dd5731b22f
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-26 00:36:37 +0900

    Add api_routing
    ```
    $ vim config/routes.rb
    ```

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     resources :users
+    resources :entries
+    resources :lectures
+    resources :rooms
+    resources :periods
+    resources :lates
   end
 end

ccc195c1969206069315665ffb7548bf0e32bae8
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-26 00:38:37 +0900

    Refactoring users_controller
    ```
    $ vim app/controllers/v1/users_controller.rb
    ```

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,24 +1,24 @@
-module V1
-  class UsersController < ApplicationController
-    protect_from_forgery with: :null_session
-    def create
-      @user = User.new
-      @user.name         = params[:name]
-      @user.grade        = params[:grade].to_i
-      @user.number       = params[:number]
-      @user.mail_address = params[:mail_address]
-      @user.password     = params[:password]
-      @user.save
+class V1::UsersController < ApplicationController
+  protect_from_forgery with: :null_session
+  def create
+    @user = User.new
+    @user.name         = params[:name]
+    @user.grade        = params[:grade].to_i
+    @user.number       = params[:number]
+    @user.mail_address = params[:mail_address]
+    @user.password     = params[:password]
+    @user.save
+    if @user.save
       respond_to do |format|
-        format.json { render json: @user}
+        format.json { render json: @user }
       end
     end
+  end
 
-    def show
-      @user = User.find(params[:id])
-      respond_to do |format|
-        format.json { render json: @user}
-      end
+  def show
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @user }
     end
   end
 end
